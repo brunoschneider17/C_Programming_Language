6a886e0af9d986bfc40dfa5f657f21d1b3aca5ac
brunoschneider17 <brunoschneider@MacBook-Pro-de-Bruno-Schneider.local>
2013-10-06 01:01:41 -0300

    Initial Commit

---
+++ C Programming Book/C Programming/C Programming/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C Programming
+//
+//  Created by Bruno Schneider on 06/10/13.
+//  Copyright (c) 2013 Bruno Schneider. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
--- C Programming Book/CElsius.c
+++ C Programming Book/CElsius.c
@@ -10,9 +10,9 @@
 
 /* Print */
 
-int main ()
+main ()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
     
     lower = 0;
@@ -21,8 +21,8 @@ int main ()
     
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0)
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
