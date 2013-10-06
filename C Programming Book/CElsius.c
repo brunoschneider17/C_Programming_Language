8f174043e2eac76c82a3c1358ef76614aef56a4f
brunoschneider17 <brunoschneider@MacBook-Pro-de-Bruno-Schneider.local>
2013-10-06 00:45:17 -0300

    Initial Commit

--- C Programming Book/CElsius.c
+++ C Programming Book/CElsius.c
@@ -10,18 +10,19 @@
 
 /* Print */
 
-main () {
-    int fhar, celsius;
+main () 
+{
+    int fahr, celsius;
     int lower, upper, step;
     
     lower = 0;
     upper = 300;
     step = 20;
     
-    fhar = lower;
-    while (fhar <= upper) {
-        celsius = 5* (fhar - 32) / 9;
-        printf("%d\t%d\n", fhar, celsius);
-        fhar = fhar + step;
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
     }
 }
